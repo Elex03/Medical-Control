7d8c32a9a9eaadee4621693e59fd7403311e7fde
Elex03 <eacuna042@gmail.com>
2024-03-12 20:02:22 -0600

    V 0.1

---
+++ src/screens/screen.js
@@ -0,0 +1,4 @@
+export {AccountScreen} from './AccountScreen'
+export {CreateAccount} from './CreateAccount'
+export {HomeStack} from '../Navigation/HomeStack'
+export {SettingScreens} from './SettingScreens'
